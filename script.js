a26cf9b0cd2519852fe9712e98deec0b99d6c268
Manu <emanuel-chocobar1@hotmail.com>
2022-12-20 18:37:14 -0300

    clase 03 completada

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const btn =  document.querySelector("[data-form-btn]"); //data attributes
 const createTask = (evento) =>{
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li');
+    task.classList.add("card"); //agrega un clase al elemento
+    input.value = '';
+    //templates strings: permite poner código html entre backticks
+    const content = 
+    `
+        <div>
+            <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>
+    `
+    task.innerHTML = content; //inyecta dinamicamente un html
+    list.appendChild(task);
+    console.log(content);
 };
 
 console.log(btn);
 //listener
 btn.addEventListener("click", createTask);
+
